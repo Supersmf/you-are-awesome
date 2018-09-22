454ff5d58e7d03c152451e9bcae3a5b2cae4f234
Supersmf <Supersmf@mail.ru>
2018-09-22 18:22:56 +0300

    resolved part of tasks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
+const createEnumerableProperty = () => { };
+
+const createNotEnumerableProperty = (e) => {
+    Object.defineProperty(Object.prototype, e, {
+        enumerable: false,
+        get: () => Object.prototype.e,
+        set: (value) => Object.prototype.e = value
+    });
+    return e;
+};
+
+
+const createProtoMagicObject = () => {
+    let res = Object.create(Object);
+    res.__proto__ = res.protorype;
+    return res;
+};
+
+//const magicObj = createProtoMagicObject();
+
+//console.log(typeof magicObj); //, 'object');
+//console.log(magicObj.__proto__); //, magicObj.prototype);
+//console.log(magicObj.prototype); //, magicObj.prototype);
+
+const incrementor = () => {
+    incrementor.valueOf = () => incrementor.result;
+    incrementor.result = ++incrementor.result || 1;
+    return incrementor;
+};
+
+const asyncIncrementor = ((count = 0) => () => ++count)();
+
+
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
